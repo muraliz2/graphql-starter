14e9f8e5e369948007ca0d064313d1254354f85a
Murali <muralismail4u@gmail.com>
2018-11-08 12:06:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema/schema')
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql:true
+}))
+
+app.listen(4000,  ()=>{
+    console.log("server listening to 4000")
+})
